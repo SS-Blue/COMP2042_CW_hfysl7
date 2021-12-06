ef6988e54ac43e68bf24d1d7dd784ae115acb756
StarryNight-Blue <starrynightbluee@gmail.com>
2021-12-06 23:35:42 +0800

    Simplify code by removing makeSingleTypeLevel class

--- src/test/Levels.java
+++ src/test/Levels.java
@@ -9,46 +9,6 @@ public class Levels {
     private static final int STEEL = 2;
     private static final int CEMENT = 3;
 
-    private static Brick[] makeSingleTypeLevel(Rectangle drawArea, int brickCount, int lineCount, double brickSizeRatio, int type) {
-        /*
-          if brickCount is not divisible by line count,brickCount is adjusted to the biggest
-          multiple of lineCount smaller then brickCount
-         */
-        brickCount -= brickCount % lineCount;
-
-        int brickOnLine = brickCount / lineCount;
-
-        double brickLen = drawArea.getWidth() / brickOnLine;
-        double brickHgt = brickLen / brickSizeRatio;
-
-        brickCount += lineCount / 2;
-
-        Brick[] tmp = new Brick[brickCount];
-
-        Dimension brickSize = new Dimension((int) brickLen, (int) brickHgt);
-        Point p = new Point();
-
-        int i;
-        for (i = 0; i < tmp.length; i++) {
-            int line = i / brickOnLine;
-            if (line == lineCount)
-                break;
-            double x = (i % brickOnLine) * brickLen;
-            x = (line % 2 == 0) ? x : (x - (brickLen / 2));
-            double y = (line) * brickHgt;
-            p.setLocation(x, y);
-            tmp[i] = makeBrick(p, brickSize, type);
-        }
-
-        for (double y = brickHgt; i < tmp.length; i++, y += 2 * brickHgt) {
-            double x = (brickOnLine * brickLen) - (brickLen / 2);
-            p.setLocation(x, y);
-            tmp[i] = new ClayBrick(p, brickSize);
-        }
-        return tmp;
-
-    }
-
     private static Brick[] makeChessboardLevel(Rectangle drawArea, int brickCount, int lineCount, double brickSizeRatio, int typeA, int typeB) {
         /*
           if brickCount is not divisible by line count,brickCount is adjusted to the biggest
@@ -96,7 +56,7 @@ public class Levels {
 
     public static Brick[][] makeLevels(Rectangle drawArea, int brickCount, int lineCount, double brickDimensionRatio) {
         Brick[][] tmp = new Brick[LEVELS_COUNT][];
-        tmp[0] = makeSingleTypeLevel(drawArea, brickCount, lineCount, brickDimensionRatio, CLAY);
+        tmp[0] = makeChessboardLevel(drawArea, brickCount, lineCount, brickDimensionRatio, CLAY, CLAY);
         tmp[1] = makeChessboardLevel(drawArea, brickCount, lineCount, brickDimensionRatio, CLAY, CEMENT);
         tmp[2] = makeChessboardLevel(drawArea, brickCount, lineCount, brickDimensionRatio, CLAY, STEEL);
         tmp[3] = makeChessboardLevel(drawArea, brickCount, lineCount, brickDimensionRatio, STEEL, CEMENT);
